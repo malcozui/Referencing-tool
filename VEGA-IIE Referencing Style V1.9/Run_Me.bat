7c1eb3c2f7cee03ab8901959d27761402898ced3
malcozui <malcozui@gmail.com>
2022-05-27 14:31:34 +0200

    .bat now creates dir if non-existant

--- VEGA-IIE Referencing Style V1.9/Run_Me.bat
+++ VEGA-IIE Referencing Style V1.9/Run_Me.bat
@@ -1,1 +1,3 @@
-@move "Vega-IIE Referencing Style V1.9.xsl" "%appdata%\Microsoft\Bibliography\Style\"
+@echo off
+if not exist "%appdata%\Microsoft\Bibliography\Style\" ( MD "%appdata%\Microsoft\Bibliography\Style\" )
+move "Vega-IIE Referencing Style V1.9.xsl" "%appdata%\Microsoft\Bibliography\Style\"
