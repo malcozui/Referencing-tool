15fbfc31a46431f3ef4d8eb2f4966689aa60aa45
Malcom Thonger <malcozui@gmail.com>
2022-09-15 08:23:58 +0200

    Fixed version number

--- VEGA-IIE Referencing Style V1.10/Run_Me.bat
+++ VEGA-IIE Referencing Style V1.10/Run_Me.bat
@@ -1,3 +1,3 @@
 @echo off
 if not exist "%appdata%\Microsoft\Bibliography\Style\" ( MD "%appdata%\Microsoft\Bibliography\Style\" )
-move "Vega-IIE Referencing Style V1.9.xsl" "%appdata%\Microsoft\Bibliography\Style\"
+move "Vega-IIE Referencing Style V1.10.xsl" "%appdata%\Microsoft\Bibliography\Style\"
